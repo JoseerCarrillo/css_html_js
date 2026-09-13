e9af9552fe0ce24926bc4d72bb9248a54af4a903
USUARIO <joseersneidercg@ufps.edu.co>
2026-09-13 15:42:22 -0500

    Actividad 3 hecha, no logra realizar el get, retornando error 404 porque no se puede encontrar la informacion

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ async function cargarUsuarios() {
  try {
  mensaje.textContent = "Cargando usuarios...";
  const response = await fetch(
- "https://jsonplaceholder.typicode.com/users"
+ "https://jsonplaceholder.typicode.com/users_no_existens"
  );
  if (!response.ok) {
  throw new Error(`Error HTTP: ${response.status}`);
